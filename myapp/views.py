9b0608f8098e0f0aa47037ddb8d6261f99ff6870
Vicente <vicentewang1009@gmail.com>
2026-09-24 14:27:07 +0800

    merge update1

--- myapp/views.py
+++ myapp/views.py
@@ -3,10 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-<<<<<<< HEAD
 def bweb(request):
     return HttpResponse("Hello, This is the bweb view, 完˙成")
-=======
 def aweb(request):
     return HttpResponse("Hello, This is the aweb view!, FINISHED")
->>>>>>> origin/master
