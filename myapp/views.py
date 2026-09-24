43acb5bd3a291b79142bf6812468ec781d831333
Vicente <vicentewang1009@gmail.com>
2026-09-24 11:29:06 +0800

    A-branch aweb finish

--- myapp/views.py
+++ myapp/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def aweb(request):
-    return HttpResponse("Hello, This is the aweb view!")
+    return HttpResponse("Hello, This is the aweb view!, FINISHED")
