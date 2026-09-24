0d426ab0b22d076d002ba49298851c21d012690b
Vicente <vicentewang1009@gmail.com>
2026-09-24 11:28:01 +0800

    A-branch aweb  update1

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def aweb(request):
+    return HttpResponse("Hello, This is the aweb view!")
